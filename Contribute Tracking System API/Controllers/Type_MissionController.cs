1f79f805cbed1a193b08fa2a01cf84b2cae0c15f
cuong20698 <52898543+cuong20698@users.noreply.github.com>
2020-03-12 11:03:01 +0700

    Up code Create a new type mission

--- Contribute Tracking System API/Controllers/Type_MissionController.cs
+++ Contribute Tracking System API/Controllers/Type_MissionController.cs
@@ -27,10 +27,32 @@ namespace Contribute_Tracking_System_API.Controllers
             return "value";
         }
 
-        
-       // POST: Type_mission
-        public void Post([FromBody]string value)
+
+        [Route("Type_Mission")]
+        [HttpPost]
+        // POST: Type_Mission?apiKey=MjE0MTQyNDI0MQ==
+        public IHttpActionResult Post([FromBody] TYPE_MISSION type_mission, [FromUri]string apiKey)
         {
+            var check = db.EMPLOYEEs.Where(x => x.apiKey == apiKey).Count();
+            if (check > 0)
+            {
+                var checkID = db.TYPE_MISSIONs.Where(x => x.id_type == type_mission.id_type).Count();
+                if (checkID > 0)
+                {
+                    return Ok(new { message = "Loại nhiệm vụ đã tồn tại!" });
+                }
+                else
+                {
+                    db.TYPE_MISSIONs.InsertOnSubmit(type_mission);
+                    db.SubmitChanges();
+                    return Ok(new { message = "Thêm loại thành công!" });
+                }
+            }
+            else
+            {
+                return Ok(new { message = "Thêm loại không thành công!" });
+            }
+
         }
 
 
