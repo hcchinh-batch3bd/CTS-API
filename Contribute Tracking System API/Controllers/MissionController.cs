4267a09b08c2d4b63d04148b7d332d8c23c1b9d3
Bao Dang <dangbao3151998@gmail.com>
2020-03-12 16:24:33 +0700

    Api Search Mission

--- Contribute Tracking System API/Controllers/MissionController.cs
+++ Contribute Tracking System API/Controllers/MissionController.cs
@@ -17,6 +17,112 @@ namespace Contribute_Tracking_System_API.Controllers
     public class MissionController : ApiController
     {
         private APIDataClassesDataContext db = new APIDataClassesDataContext();
+        //Get List Mission
+        [Route("Mission/ListMissionComplete")]
+        [HttpGet]
+        public IHttpActionResult GetListMissonComplete([FromUri] string apiKey)
+        {
+            if (apiKey != null)
+            {
+                string _message = "Danh sách nhiệm vụ hoàn thành của một nhân viên";
+                bool _status = true;
+
+                var key = (from a in db.EMPLOYEEs
+                           where (a.apiKey == apiKey)
+                           join b in db.MISSION_PROCESSes on a.id_employee equals b.id_employee
+                           join c in db.MISSIONs on b.id_mission equals c.id_mission
+                           where (b.status == 1)
+                           select new {b.id_mission,c.name_mission, c.point, b.date}).ToList();
+
+                if (!key.Any())
+                    _message = "Nhân viên chưa hoàn thành nhiệm vụ nào hết !";
+
+                return Ok(new { results = key, status = _status, message = _message });
+            }
+            else
+                return Ok(new { results = "", status = "false", message = "Not Found apiKey" });
+
+        }
+        //Get Describe Mission
+        [Route("Mission/{id}/Describe")]
+        [HttpGet]
+        public IHttpActionResult GetDescribeMission(int id, [FromUri] string apiKey)
+        {
+            if (apiKey != null)
+            {
+                string message = "Chi tiết của một nhiệm vụ";
+                bool status = true;
+                var key = db.MISSIONs.Where(s => s.id_mission == id).Select(m=> new { m.id_mission, m.name_mission, m.Stardate, m.point, m.exprie, m.describe, m.id_type, m.id_employee });
+                if (!key.Any())
+                    message = "Không có chi tiết nhiệm vụ nào hết !";
+                return Ok(new { results = key, status = status, message = message });
+            }
+            else
+                return Ok(new { results = "", status = "Flase", message = "Not Found apiKey" });
+
+        }
+        //Post Mission
+        [Route("Mission/Post")]
+        [HttpPost]
+        public IHttpActionResult PostMission(MISSION mission, [FromUri] string apiKey)
+        {
+            if (apiKey != null)
+            {
+                var check = db.EMPLOYEEs.Where(s => s.apiKey.Equals(apiKey) && s.level_employee==true).Select(x => x.id_employee).Count();
+                string messeage = "Tạo nhiệm vụ thành công !";
+                var key = db.EMPLOYEEs.Where(s => s.apiKey == apiKey);
+                db.MISSIONs.InsertOnSubmit(mission);
+                db.SubmitChanges();
+                return Ok(new { message = messeage });
+            }
+            else
+                return Ok(new { message = "Not Found apiKey" });
+        }
+
+        //Confim Mission of Admin
+        [Route("Mission/{id}/Confim")]
+        [HttpPut]
+        public IHttpActionResult ConfimMission(int id, [FromUri] string apiKey)
+        {
+            if (apiKey != null)
+            {
+                var check = db.EMPLOYEEs.Where(s => s.apiKey.Equals(apiKey) && s.level_employee == true).Select(x => x.id_employee).Count();
+                if (check > 0)
+                {
+                    foreach (var i in db.MISSIONs)
+                    {
+                        var cm = db.MISSIONs.Where(x => x.id_mission == id).Select(x => x).SingleOrDefault();
+                        cm.status = 1;
+                        db.SubmitChanges();
+                        break;
+                    }
+                    return Ok(new { message = "Xác nhận xét duyệt thành công !!!" });
+                }
+                else
+                    return Ok(new { message = "Không đủ quyền hạn !!!" });
+            }
+            else
+                return Ok(new { message = "Not Found apiKey" });
+        }
+        //Get Search Mission
+        [Route("Mission/Search")]
+        [HttpGet]
+        public IHttpActionResult GetSearchMission([FromUri] string key, [FromUri] string apiKey)
+        {
+            if (apiKey != null && key != null)
+            {
+                string _messeage = "Tìm kiếm thành công !";
+                bool _status = true;
+                var search = db.MISSIONs.Where(s => s.name_mission.Contains(key)).Select(a=> new { a.id_mission, a.name_mission, a.Stardate, a.point, a.exprie, a.describe, a.id_type, a.id_employee } );
+                if (!search.Any())
+
+                    _messeage = "Không tìm thấy nhiệm vụ nào !";
+                return Ok(new { result = search, status = _status, messeage = _messeage });
+            }
+            else
+                return Ok(new { results = "", status = "false", message = "Not Found apiKey" });
+
+        }
         [Route("Missison/Missionavaible")]
         [HttpGet]
         public IHttpActionResult Missonavaible()
