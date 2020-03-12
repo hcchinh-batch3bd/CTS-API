d94ffa01a8676032af9d4df4182654bfbbe6ce1c
hcchinh-batch3bd <hcchinh-batch3bd@webmail.edu.vn>
2020-03-12 08:26:12 +0700

    Add project files.

---
+++ Contribute Tracking System API/Controllers/AccountController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Text;
+using System.Security.Cryptography;
+using System.Net.Mail;
+using System.Web.Http.Cors;
+
+namespace Contribute_Tracking_System_API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*", exposedHeaders: "X-My-Header")]
+    public class AccountController : ApiController
+    {
+
+        private APIDataClassesDataContext db = new APIDataClassesDataContext();
+        [Route("Account/CheckLogin")]
+        [HttpGet]
+        public IHttpActionResult GetCheckLogin([FromUri] string id, [FromUri] string pw)
+        {
+            string message = "Bad request";
+            bool status = false;
+            if (id!=null && pw!=null)
+            {
+                message = "Đăng nhập thành công !!";
+                status = true;
+                var key = db.EMPLOYEEs.Where(x => x.id_employee == int.Parse(id) && x.password == CreateMD5Hash(pw)).Select(s => new { s.name_employee, s.point, s.apiKey });
+                if (!key.Any())
+                    message = "ID đăng nhập hoặc mật khẩu không hợp lê !!";
+                return Ok(new { results = key, status = status, message = message });
+            }
+            else return Ok(new { results = "", status = status, message = message });
+        }
+            
+           
+        [Route("Account/OTP")]
+        [HttpGet]
+        public IHttpActionResult SendOTP([FromUri] string mail)
+        {
+            string _message = "Bad request";
+            bool _status = false;
+            if (mail!=null)
+            {
+                _message = "Đăng nhập thành công !!";
+                _status = true;
+                Random rnd = new Random();
+                int otp = rnd.Next(000000, 999999);
+                string msg = "OTP để xác nhận lấy lại mật khẩu của bản : " + otp;
+                bool f = SendOTP("lvx51523@outlook.com", mail, "Xác thực mail OTP", msg);
+                if (f)
+                {
+                    _message = "Đã gửi OTP đến mail !!";
+                }
+                else { _message = "Gửi OTP thất bại, thử lại sau !! !!"; }
+            }
+            return Ok(new { results = "", status = _status, message = _message });
+
+        }
+        // GET: Account
+        public IEnumerable<EMPLOYEE> Get()
+        {
+            return db.EMPLOYEEs.ToList<EMPLOYEE>();
+        }
+        // GET: Account/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: Account
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: Account/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: Account/5
+        public void Delete(int id)
+        {
+        }
+        public string CreateMD5Hash(string input)
+        {
+            // Step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+        public bool SendOTP(string from, string to, string subject, string body)
+        {
+            bool f = false;
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.To.Add(to);
+                mailMessage.From = new MailAddress(from);
+                mailMessage.Subject = subject;
+                mailMessage.Body = body;
+                SmtpClient client = new SmtpClient();
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("lvx51523@outlook.com", "opencart123");
+                client.Port = 25; // 25 587
+                client.Host = "smtp.office365.com";
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.EnableSsl = true;
+                
+                client.Send(mailMessage);
+                f = true;
+            }
+            catch (Exception ex)
+            {
+                f = false;
+            }
+            return f;
+        }
+    }
+}
---
+++ Contribute Tracking System API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Contribute_Tracking_System_API.WebApiApplication" Language="C#" %>
